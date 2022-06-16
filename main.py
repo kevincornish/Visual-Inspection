28f4c0f6bc380a205e1c90e9e42b662adea6cef2
Kevin Cornish <kevinz_c@live.com>
2022-06-16 23:41:05 +0100

    set ROI at start of inspection

--- main.py
+++ main.py
@@ -23,48 +23,48 @@ def merge_images():
     merge1 = cv2.addWeighted(img1, 0.5, img2, 0.5, 0)
     merge2 = cv2.addWeighted(img3, 0.5, img4, 0.5, 0)
     merge3 = cv2.addWeighted(merge1, 0.5, merge2, 0.5, 0)
+    cv2.imwrite("images/merged1.jpg", merge1)
+    cv2.imwrite("images/merged2.jpg", merge2)
+    cv2.imwrite("images/merged_all.jpg", merge3)
     if debug:
-        cv2.imwrite("images/merged1.jpg", merge1)
-        cv2.imwrite("images/merged2.jpg", merge2)
-        cv2.imwrite("images/merged_all.jpg", merge3)
         print (f"4 frames merged")
 
+def set_roi():
+    merged_all = cv2.imread('images/merged_all.jpg')
+    roi = cv2.selectROI(merged_all) # select roi
+    roi_cropped = merged_all[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])] # crop image
+    cv2.imwrite("images/roi_cropped.jpg",roi_cropped) # save cropped image
 
 def particle_count():
-    im = cv2.imread('images/merged_all.jpg') #load merged frames
+    im = cv2.imread('images/roi_cropped.jpg') #load merged frames
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY) #convert to gray
     gray = cv2.GaussianBlur(gray, (3,3), 0) #add slight blur to clean edges
     maxValue = 255
     blockSize = 21 # sensitivity
     C = -23 # constant to be subtracted
     im_thresholded = cv2.adaptiveThreshold(gray, maxValue, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, blockSize, C)
-    r_o_i = im_thresholded[131:338, 81:265] # grab the region of interest we want
-    labelarray, particle_count = ndimage.label(r_o_i) # count particles found in ROI window
-    if debug:
-        cv2.imwrite("images/grey_merged.jpg", gray) # save the grayscale image
-        cv2.imwrite("images/threshold_merged.jpg", im_thresholded) # save the thresholded image
+    labelarray, particle_count = ndimage.label(im_thresholded) # count particles found in ROI window
+    cv2.imwrite("images/grayscale.jpg", gray) # save the grayscale image
+    cv2.imwrite("images/thresholded.jpg", im_thresholded) # save the thresholded image
     print (f"Particles found: {particle_count}")
 
 
 def show_images():
     im = cv2.imread('images/1.png')
     im_merged = cv2.imread('images/merged_all.jpg')
-    gray = cv2.imread('images/grey_merged.jpg')
-    im_thresholded = cv2.imread('images/threshold_merged.jpg')
-    r_o_i = im_thresholded[131:338, 81:265]
-    pylab.figure("1st Frame")
-    pylab.imshow(im)
-    pylab.figure("Merged Frames")
-    pylab.imshow(im_merged)
-    pylab.figure("Grayscale")
-    pylab.imshow(gray)
-    pylab.figure("Thresholded")
-    pylab.imshow(im_thresholded)
-    pylab.figure("ROI")
-    pylab.imshow(r_o_i)
-    pylab.show()
+    gray = cv2.imread('images/grayscale.jpg')
+    roi = cv2.imread('images/roi_cropped.jpg')
+    im_thresholded = cv2.imread('images/thresholded.jpg')
+    cv2.imshow("1st Frame", im)
+    cv2.imshow("Merged Frames", im_merged)
+    cv2.imshow("Grayscale", gray)
+    cv2.imshow("ROI", roi)
+    cv2.imshow("Thresholded", im_thresholded)
+    cv2.waitKey(0)
+
 #capture_frames(3)
 merge_images()
+set_roi()
 particle_count()
 et = time.time()
 elapsed_time = et - st
