958abcf8081460909aad2eda7a734247fe16ad33
Kevin Cornish <kevinz_c@live.com>
2022-06-17 22:19:15 +0100

    add multiple ROI for future, split settings

--- main.py
+++ main.py
@@ -1,22 +1,45 @@
 import configparser
 import tkinter
 from tkinter.messagebox import askyesno
-from particle import set_roi, merge_images, particle_count, show_images, capture_frames
+from particle import get_roi, set_roi, merge_images, particle_count, show_images, capture_frames, fill_level
 config = configparser.ConfigParser()
 config_ini = "config.ini"
 config.read(config_ini)
 
-def particle_detection():
+def fill_level_settings():
+    threshold = config.get("FILL_LEVEL_SETTING","threshold")
+    fill_level_window = tkinter.Toplevel()
+    fill_level_window.title("Fill Level Settings")
+    threshold_label = tkinter.Label(fill_level_window, text="threshold").pack()
+    threshold_entry = tkinter.Entry(fill_level_window)
+    threshold_entry.insert(0, threshold)
+    threshold_entry.pack()
+
+    def saveConfig():
+        config.set("FILL_LEVEL_SETTING", "threshold", threshold_entry.get())
+        with open(config_ini, "w") as config_file:
+            config.write(config_file)
+
+    save_button = tkinter.Button(fill_level_window, text="Save", command=saveConfig).pack()
+    close_button = tkinter.Button(fill_level_window, text="close", command=fill_level_window.destroy).pack(pady=20)
+
+def particle_settings():
     particle_blur = config.get("PARTICLE_SETTING","blur")
     particle_maxvalue = config.get("PARTICLE_SETTING","maxvalue")
     particle_blocksize = config.get("PARTICLE_SETTING","blocksize")
     particle_constant = config.get("PARTICLE_SETTING","constant")
+    particle_fromcenter = config.get("PARTICLE_SETTING","fromcenter")
     particle_window = tkinter.Toplevel()
     particle_window.title("Particle Settings")
     particle_blur_label = tkinter.Label(particle_window, text="Blur").pack()
     particle_blur_entry = tkinter.Entry(particle_window)
     particle_blur_entry.insert(0, particle_blur)
     particle_blur_entry.pack()
+
+    particle_fromcenter_label = tkinter.Label(particle_window, text="fromcenter").pack()
+    particle_fromcenter_entry = tkinter.Entry(particle_window)
+    particle_fromcenter_entry.insert(0, particle_fromcenter)
+    particle_fromcenter_entry.pack()
 
     particle_maxvalue_label = tkinter.Label(particle_window, text="maxvalue").pack()
     particle_maxvalue_entry = tkinter.Entry(particle_window)
@@ -35,6 +58,7 @@ def particle_detection():
 
     def saveConfig():
         config.set("PARTICLE_SETTING", "blur", particle_blur_entry.get())
+        config.set("PARTICLE_SETTING", "fromcenter", particle_fromcenter_entry.get())
         config.set("PARTICLE_SETTING", "maxvalue", particle_maxvalue_entry.get())
         config.set("PARTICLE_SETTING", "blocksize", particle_blocksize_entry.get())
         config.set("PARTICLE_SETTING", "constant", particle_constant_entry.get())
@@ -42,20 +66,27 @@ def particle_detection():
             config.write(config_file)
 
     save_button = tkinter.Button(particle_window, text="Save", command=saveConfig).pack()
+    close_button = tkinter.Button(particle_window, text="close", command=particle_window.destroy).pack(pady=20)
 
-    particle_window_label = tkinter.Label(particle_window, text="Particle Detection").pack()
-    particle_capture_button = tkinter.Button(particle_window, text="Capture Image", command=capture_frames).pack()
-    particle_merge_button = tkinter.Button(particle_window, text="Merge Images", command=merge_images).pack()
-    particle_roi_button = tkinter.Button(particle_window, text="Set ROI", command=set_roi).pack()
-    particle_analysis_button = tkinter.Button(particle_window, text="Analyse", command=particle_count).pack()
-    particle_show_button = tkinter.Button(particle_window, text="Show Result", command=show_images).pack()
-    close_button = tkinter.Button(particle_window, text="Close", command=particle_window.destroy).pack(pady=20)
+
+def detection():
+    detection_window = tkinter.Toplevel()
+    detection_window.title("Detection")
+    detection_window_label = tkinter.Label(detection_window, text="Particle Detection").pack()
+    detection_capture_button = tkinter.Button(detection_window, text="Capture Image", command=capture_frames).pack()
+    detection_merge_button = tkinter.Button(detection_window, text="Merge Images", command=merge_images).pack()
+    detection_get_roi_button = tkinter.Button(detection_window, text="Crop ROI", command=get_roi).pack()
+    detection_reset_roi_button = tkinter.Button(detection_window, text="Reset ROI", command=set_roi).pack()
+    detection_fill_level_button = tkinter.Button(detection_window, text="Find Fill Level", command=fill_level).pack()
+    particle_analysis_button = tkinter.Button(detection_window, text="Find Particle", command=particle_count).pack()
+    particle_show_button = tkinter.Button(detection_window, text="Show Result", command=show_images).pack()
+    close_button = tkinter.Button(detection_window, text="Close", command=detection_window.destroy).pack(pady=20)
 
 def general_settings():
     debug = config.get("SETTINGS","debug")
     frames = config.get("SETTINGS","frames")
     general_window = tkinter.Toplevel()
-    general_label = tkinter.Label(master=general_window, text="General Settings").pack()
+    general_window.title("General Settings")
 
     frames_label = tkinter.Label(general_window, text="frames").pack()
     frames_entry = tkinter.Entry(general_window)
@@ -78,8 +109,10 @@ def general_settings():
 
 
 button_window = tkinter.Tk()
-particle_detection_button = tkinter.Button(button_window, text="Particle Detection", command=particle_detection).pack()
+detection_button = tkinter.Button(button_window, text="Detection", command=detection).pack()
 general_settings_button = tkinter.Button(button_window, text="General Settings", command=general_settings).pack()
+particle_settings_button = tkinter.Button(button_window, text="Particle Settings", command=particle_settings).pack()
+fill_level_settings_button = tkinter.Button(button_window, text="Fill Level Settings", command=fill_level_settings).pack()
 
 def close():
     answer = askyesno(title='confirmation', message='Are you sure that you want to quit?')
@@ -94,6 +127,14 @@ def close():
             pass
         try:
             button_window.destroy()
+        except:
+            pass
+        try:
+            detection_window.destroy()
+        except:
+            pass
+        try:
+            fill_level_window.destroy()
         except:
             pass
 
--- particle.py
+++ particle.py
@@ -24,14 +24,14 @@ def capture_frames():
         config.read(config_ini)
         frames = config.getint("SETTINGS", "frames")
         debug = config.getboolean("SETTINGS", "debug")
-        if debug:
+        if debug == True:
             st = time.time()
         frame = 0
         while frame < frames: # loop until taken enough frames
             frame += 1
             result, image = camera.read() # read from camera
             cv2.imwrite(f"{img_path}{frame}.png", image) # save images
-        if debug:
+        if debug == True:
             print(f"{frame} frames captured")
             et = time.time()
             elapsed_time = et - st
@@ -40,7 +40,7 @@ def capture_frames():
 def merge_images():
     config.read(config_ini)
     debug = config.getboolean("SETTINGS", "debug")
-    if debug:
+    if debug == True:
         st = time.time()
     img1 = cv2.imread(f'{img_path}1.png')
     img2 = cv2.imread(f'{img_path}2.png')
@@ -56,7 +56,7 @@ def merge_images():
     except:
         showerror(title="Error", message="Capture image first")
     else:
-        if debug:
+        if debug == True:
             print (f"4 frames merged")
             et = time.time()
             elapsed_time = et - st
@@ -65,42 +65,75 @@ def merge_images():
 def set_roi():
     config.read(config_ini)
     debug = config.getboolean("SETTINGS", "debug")
+    fromcenter = config.getboolean("PARTICLE_SETTING", "fromcenter")
     merged_all = cv2.imread(f'{img_path}merged_all.jpg')
-    try:
-        roi = cv2.selectROI(merged_all) # select roi
-    except:
-        showerror(title="Error", message="Merge images first")
-    else:
-        roi_cropped = merged_all[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])] # crop image
-        edit_config("PARTICLE_ROI", "y1", str(roi[1]))
-        edit_config("PARTICLE_ROI", "y2", str(roi[1]+roi[3]))
-        edit_config("PARTICLE_ROI", "x1", str(roi[0]))
-        edit_config("PARTICLE_ROI", "x2", str(roi[0]+roi[2]))
-        cv2.imwrite(f"{img_path}roi_cropped.jpg",roi_cropped) # save cropped image
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+    roi = cv2.selectROIs("test", merged_all, fromcenter) # select roi
+    #showerror(title="Error", message="Merge images first")
+    roi_cropped = merged_all[int(roi[0][1]):int(roi[0][1]+roi[0][3]), int(roi[0][0]):int(roi[0][0]+roi[0][2])] # crop image
+    roi_cropped2 = merged_all[int(roi[1][1]):int(roi[1][1]+roi[1][3]), int(roi[1][0]):int(roi[1][0]+roi[1][2])] # crop image
+    edit_config("PARTICLE_ROI", "y1", str(roi[0][1]))
+    edit_config("PARTICLE_ROI", "y2", str(roi[0][1]+roi[0][3]))
+    edit_config("PARTICLE_ROI", "x1", str(roi[0][0]))
+    edit_config("PARTICLE_ROI", "x2", str(roi[0][0]+roi[0][2]))
+    edit_config("FILL_LEVEL_ROI", "y1", str(roi[1][1]))
+    edit_config("FILL_LEVEL_ROI", "y2", str(roi[1][1]+roi[1][3]))
+    edit_config("FILL_LEVEL_ROI", "x1", str(roi[1][0]))
+    edit_config("FILL_LEVEL_ROI", "x2", str(roi[1][0]+roi[1][2]))
+    if debug == True:
+        print(f"PARTICLE - Y1: {int(roi[0][1])} Y2{int(roi[0][1]+roi[0][3])} X1: {int(roi[0][0])} X2: {int(roi[0][0]+roi[0][2])}")
+        print(f"FILL LEVEL - Y1: {int(roi[1][1])} Y2{int(roi[1][1]+roi[1][3])} X1: {int(roi[1][0])} X2: {int(roi[1][0]+roi[1][2])}")
+    cv2.imwrite(f"{img_path}roi_cropped.jpg",roi_cropped) # save cropped image
+    cv2.imwrite(f"{img_path}roi_cropped_fill.jpg",roi_cropped2) # save cropped image
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 def get_roi():
     config.read(config_ini)
     debug = config.getboolean("SETTINGS", "debug")
-    if debug:
+    if debug == True:
         st = time.time()
     merged_all = cv2.imread(f'{img_path}merged_all.jpg')
-    y1 = config.getint("PARTICLE_ROI", "y1")
-    y2 = config.getint("PARTICLE_ROI", "y2")
-    x1 = config.getint("PARTICLE_ROI", "x1")
-    x2 = config.getint("PARTICLE_ROI", "x2")
-    roi_cropped = merged_all[y1:y2, x1:x2] # crop image
+    particle_y1 = config.getint("PARTICLE_ROI", "y1")
+    particle_y2 = config.getint("PARTICLE_ROI", "y2")
+    particle_x1 = config.getint("PARTICLE_ROI", "x1")
+    particle_x2 = config.getint("PARTICLE_ROI", "x2")
+    fill_y1 = config.getint("FILL_LEVEL_ROI", "y1")
+    fill_y2 = config.getint("FILL_LEVEL_ROI", "y2")
+    fill_x1 = config.getint("FILL_LEVEL_ROI", "x1")
+    fill_x2 = config.getint("FILL_LEVEL_ROI", "x2")
+    roi_cropped = merged_all[particle_y1:particle_y2, particle_x1:particle_x2] # crop image to particle ROI
+    roi_cropped_fill = merged_all[fill_y1:fill_y2, fill_x1:fill_x2] # crop image to fill level ROI
     cv2.imwrite(f"{img_path}roi_cropped.jpg",roi_cropped) # save cropped image
-    if debug:
+    cv2.imwrite(f"{img_path}roi_cropped_fill.jpg",roi_cropped_fill) # save cropped image
+    if debug == True:
         et = time.time()
         elapsed_time = et - st
         print('Execution time (get_roi):', elapsed_time, 'seconds')
 
+def fill_level():
+    if debug == True:
+        st = time.time()
+    im = cv2.imread(f'{img_path}roi_cropped_fill.jpg')
+    threshold = config.getint("FILL_LEVEL_SETTING", "threshold")
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY) # convert to gray
+    if debug == True:
+        print('height, width:', gray.shape) #height and width of image
+    ret, thresholded = cv2.threshold(gray, threshold, 255, cv2.THRESH_BINARY)
+    x, y, width, height = cv2.boundingRect(thresholded)
+    text = f"Height: {height} Width: {width}"
+    text_position = (5,15)
+    cv2.putText(thresholded, text, text_position, fontFace = cv2.FONT_HERSHEY_SIMPLEX, fontScale = 0.5, color = (250,225,100))
+    cv2.imwrite(f"{img_path}thresholded_fill.jpg",thresholded)
+    if debug == True:
+        et = time.time()
+        elapsed_time = et - st
+        print('Execution time (fill_level):', elapsed_time, 'seconds')
+        print(f"Height: {height} Width: {width}")
+
 def particle_count():
     config.read(config_ini)
     debug = config.getboolean("SETTINGS", "debug")
-    if debug:
+    if debug == True:
         st = time.time()
     blur = config.getint("PARTICLE_SETTING", "blur")
     maxValue = config.getint("PARTICLE_SETTING", "maxValue")
@@ -124,14 +157,17 @@ def particle_count():
             et = time.time()
             elapsed_time = et - st
             print('Execution time (analysis):', elapsed_time, 'seconds')
+            print(f"Particles: {particle_count}")
 
 def show_images():
     config.read(config_ini)
     im = cv2.imread(f'{img_path}1.png')
     im_thresholded = cv2.imread(f'{img_path}thresholded.jpg')
+    fill_level = cv2.imread(f'{img_path}thresholded_fill.jpg')
     try:
-        cv2.imshow("1st Frame", im)
-        cv2.imshow("Thresholded", im_thresholded)
+        cv2.imshow("Original", im)
+        cv2.imshow("Particle", im_thresholded)
+        cv2.imshow("Fill Level", fill_level)
     except:
         showerror(title="Error", message="Image not taken / analysed")
     else:
