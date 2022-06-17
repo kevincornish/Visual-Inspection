2f8c968a0102271609ce1fe7a015a770a6153494
Kevin Cornish <kevinz_c@live.com>
2022-06-17 08:38:34 +0100

    add config ini, save settings to config

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ import cv2
 import pylab
 from scipy import ndimage
 import time
-st = time.time()
+import configparser
+
+config = configparser.ConfigParser()
+config_ini = "config.ini"
+config.read(config_ini)
 camera = cv2.VideoCapture(0)
-debug = True
+debug = config.get("SETTINGS", "debug")
+frames = config.get("SETTINGS", "frames")
+
+def edit_config(setting, parameter, value):
+    config.set(setting, parameter, value)
+    with open(config_ini, "w") as config_file:
+        config.write(config_file)
 
 def capture_frames(frames):
+    if debug:
+        st = time.time()
     frame = 0
     while frame < frames: # loop until taken enough frames
         frame += 1
@@ -14,8 +26,13 @@ def capture_frames(frames):
         cv2.imwrite(f"images/{frame}.png", image) # save images
     if debug:
         print(f"{frame} frames captured")
+        et = time.time()
+        elapsed_time = et - st
+        print('Execution time (capture):', elapsed_time, 'seconds')
 
 def merge_images():
+    if debug:
+        st = time.time()
     img1 = cv2.imread('images/1.png')
     img2 = cv2.imread('images/2.png')
     img3 = cv2.imread('images/3.png')
@@ -28,26 +45,39 @@ def merge_images():
     cv2.imwrite("images/merged_all.jpg", merge3)
     if debug:
         print (f"4 frames merged")
+        et = time.time()
+        elapsed_time = et - st
+        print('Execution time (merge):', elapsed_time, 'seconds')
 
 def set_roi():
     merged_all = cv2.imread('images/merged_all.jpg')
     roi = cv2.selectROI(merged_all) # select roi
     roi_cropped = merged_all[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])] # crop image
+    edit_config("ROI", "y1", str(roi[1]))
+    edit_config("ROI", "y2", str(roi[1]+roi[3]))
+    edit_config("ROI", "x1", str(roi[0]))
+    edit_config("ROI", "x2", str(roi[0]+roi[2]))
     cv2.imwrite("images/roi_cropped.jpg",roi_cropped) # save cropped image
 
 def particle_count():
+    if debug:
+        st = time.time()
+    blur = config.getint("PARTICLE", "blur")
+    maxValue = config.getint("PARTICLE", "maxValue")
+    blockSize = config.getint("PARTICLE", "blockSize")# sensitivity
+    constant = config.getint("PARTICLE", "constant") # constant to be subtracted
     im = cv2.imread('images/roi_cropped.jpg') #load merged frames
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY) #convert to gray
-    gray = cv2.GaussianBlur(gray, (3,3), 0) #add slight blur to clean edges
-    maxValue = 255
-    blockSize = 21 # sensitivity
-    C = -23 # constant to be subtracted
-    im_thresholded = cv2.adaptiveThreshold(gray, maxValue, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, blockSize, C)
+    gray = cv2.GaussianBlur(gray, (blur,blur), 0) #add slight blur to clean edges
+    im_thresholded = cv2.adaptiveThreshold(gray, maxValue, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, blockSize, constant)
     labelarray, particle_count = ndimage.label(im_thresholded) # count particles found in ROI window
     cv2.imwrite("images/grayscale.jpg", gray) # save the grayscale image
     cv2.imwrite("images/thresholded.jpg", im_thresholded) # save the thresholded image
     print (f"Particles found: {particle_count}")
-
+    if debug:
+        et = time.time()
+        elapsed_time = et - st
+        print('Execution time (analyse):', elapsed_time, 'seconds')
 
 def show_images():
     im = cv2.imread('images/1.png')
@@ -62,11 +92,8 @@ def show_images():
     cv2.imshow("Thresholded", im_thresholded)
     cv2.waitKey(0)
 
-#capture_frames(3)
+#capture_frames(frames)
 merge_images()
 set_roi()
 particle_count()
-et = time.time()
-elapsed_time = et - st
-print('Execution time:', elapsed_time, 'seconds')
 show_images()
